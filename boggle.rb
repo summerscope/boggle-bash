625945423530b666646f6c06d6d92f8c1f17c3f8
Laura Summers <summerscope@gmail.com>
2017-12-30 12:40:23 +1100

    Initial commit

---
+++ boggle.rb
@@ -0,0 +1,27 @@
+file  = File.open("cubes.txt")
+lines = file.readlines.map{|x| x.chomp}
+cubes = lines.map{|x| x.scan(/Qu|[A-Z]/)}
+
+def shuffleboard(cubes)
+  cubes = cubes.shuffle
+  print "+--------------+\n"
+  4.times do
+    print "|  "
+    4.times do
+      letter = cubes.pop.sample
+      if letter == "Qu" then
+        print letter, " " 
+      else 
+        print letter, "  "
+      end 
+    end
+    print "|\n"
+  end
+  print "+--------------+\n"
+end
+
+def playgame(cubes, duration=90)
+  shuffleboard cubes
+  sleep duration
+  puts "game over, bitches!"
+end
